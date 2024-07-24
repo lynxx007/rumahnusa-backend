35159a05426f959729e755012261e8aafeec4256
Dimas <dimasekaputra2000@gmail.com>
2024-07-24 09:16:36 +0700

    feat: permission

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
+import { PermissionModule } from './permission/permission.module';
 
 @Module({
-  imports: [AuthModule, UserModule, RoleModule],
+  imports: [AuthModule, UserModule, RoleModule, PermissionModule],
   controllers: [AppController],
   providers: [AppService],
 })
