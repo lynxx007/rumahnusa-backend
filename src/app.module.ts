36cffd6b1574e561c0193a139ff229eb39868567
Dimas <dimasekaputra2000@gmail.com>
2024-07-24 09:14:54 +0700

    feat: role

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
