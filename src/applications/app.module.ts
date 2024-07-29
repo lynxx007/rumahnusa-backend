6ca1395d6eb584063e8778dcdee6cf3ed4f73d6e
Dimas <dimasekaputra2000@gmail.com>
2024-07-29 10:56:48 +0700

    feat: database connection inf

--- src/applications/app.module.ts
+++ src/applications/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//Database Configuration
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_CONFIG } from 'src/config/database';
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(DB_CONFIG)],
   controllers: [AppController],
   providers: [AppService],
 })
