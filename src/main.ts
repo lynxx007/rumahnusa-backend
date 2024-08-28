01f1d918210168b78263bcf9c5db8184a3df74be
Dimas <dimasekaputra2000@gmail.com>
2024-08-28 13:27:40 +0700

    feat: allow cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector))
