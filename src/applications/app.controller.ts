27a5348f979381121bb8b6846503b403857cd0ea
Luthfi Rizky <luthfirizky7@gmail.com>
2025-01-03 10:27:48 +0700

    update

--- src/applications/app.controller.ts
+++ src/applications/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+ 
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+ @Get('all')
+  async getAll() {
+    return await this.appService.getAll();
+  }
+
+  @Get('listing')
+  async fetchListing(@Query('offset') offset: number, @Query('limit') limit: number) {
+    return await this.appService.fetchListing(offset, limit);
+  }
+  
+  @Get('search')
+  async search(@Query('searchTerm') searchTerm: string) {
+    return await this.appService.search(searchTerm);
+  }
+
+  @Get('location')
+  async getByCurrentLocation(@Query('lat') lat: number, @Query('lon') lon: number) {
+    return await this.appService.getByCurrentLocation(lat, lon);
+  }
+
+  @Get('map')
+  async getByMapCenter(@Query('latNortheast') latNortheast: number, @Query('lonNortheast') lonNortheast: number, @Query('latSouthwest') latSouthwest: number, @Query('lonSouthwest') lonSouthwest: number) {
+    return await this.appService.getByMapCenter(latNortheast, lonNortheast, latSouthwest, lonSouthwest);
   }
 }
--- src/applications/app.service.ts
+++ src/applications/app.service.ts
@@ -1,8 +1,85 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { create, insertMultiple, search } from '@orama/orama';
+import * as data from '../json/new-data.json';
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnModuleInit {
+    private db: any
+
+    
+  async onModuleInit() {
+      this.db = create({
+        schema: {
+            market_title: "string",
+            _geoloc: "geopoint",
+            image: "string[]",
+            price: "number",
+            listing_id: "string",
+        }
+      })
+      try {
+        //@ts-ignore
+        await insertMultiple(this.db, data);
+        console.log("data inserted");
+      } catch (error) {
+        console.log('error while inserting data',error);
+      }
+      
+  }
+
+  async getAll() {
+    return await search(this.db,{
+        limit: 1000
+    });
+  }
+  async search(searchTerm: string) {
+    return await search(this.db, {
+        term: searchTerm,
+        limit: 10
+    });
+  }
+
+  async getByCurrentLocation(lat: number, lon: number) {
+    return await search(this.db, {
+        limit: 1000,
+        where: {
+            _geoloc: {
+                radius: {
+                    coordinates: {
+                        lat: lat,
+                        lon: lon,
+                      },
+                      value: 100000,
+                      inside: true,
+                      unit: "km",
+                }
+            }
+        }
+    })
+  }
+
+  async getByMapCenter(latNortheast: number, lonNortheast: number, latSouthwest: number, lonSouthwest: number) {
+    return await search(this.db, {
+        limit: 1000,
+        where: {
+          _geoloc: {
+            // Use polygon to match exact map boundaries
+            polygon: {
+              coordinates: [
+                { lat: latNortheast, lon: lonNortheast },
+                { lat: latSouthwest, lon: lonNortheast },
+                { lat: latSouthwest, lon: lonSouthwest },
+                { lat: latNortheast, lon: lonSouthwest },
+                { lat: latNortheast, lon: lonNortheast },
+              ],
+            },
+          },
+        },
+    });
+  }
+  async fetchListing(offset: number, limit: number) {
+    return await search(this.db, {
+        limit: limit,
+        offset: offset
+    })
   }
 }
